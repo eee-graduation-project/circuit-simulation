06b03bdd4a6a5944a592b2ea8504f2506695be07
5tarry <hacherry05@naver.com>
2024-04-03 01:33:13 +0900

    ⚒️: env 설정

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,10 +11,18 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
+import os
+
+env = environ.Env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
+SERVER_URL = env('SERVER_URL')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
