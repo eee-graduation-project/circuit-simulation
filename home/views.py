bed74d5c8d4f7aeacdcd35dfedd59fb3073e31be
5tarry <hacherry05@naver.com>
2024-04-03 01:43:11 +0900

    ✨: home 페이지 렌더링

--- home/views.py
+++ home/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.conf import settings
 
 # Create your views here.
+def show_home(request):
+  return render(request, 'home/test.html')
+
+def test_example(request):
+  # TODO process request
+  return render(request, 'home/test.html')
