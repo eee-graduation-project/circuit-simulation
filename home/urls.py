5606d15128b3b084c287410dd414df646ec56195
5tarry <hacherry05@naver.com>
2024-05-06 20:23:39 +0900

    🐛: 테스트 페이지 주석 처리

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('test/', views.test_example),
+    # path('test/', views.test_example),
     path('', views.show_home),
 ]
