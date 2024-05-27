0dee80c7f8f695c7684dbb89ae00a2cfe82448e2
5tarry <hacherry05@naver.com>
2024-05-28 02:16:10 +0900

    ✨: library에서 element 추가 시 양끝에 작은 wire 연결

--- home/static/draganddrop.js
+++ home/static/draganddrop.js
@@ -1,59 +1,66 @@
+import {getSVGCoordinates} from "./utils.js";
+import {boardDragStart, startWire} from "./drag-board.js";
+
 const board = document.querySelector(".board");
 const elements = document.querySelectorAll(".library__element_item img");
 
-let offsetX, offsetY, state;
-let e;
-
-board.addEventListener("drop", drop);
-board.addEventListener("dragover", dragOver);
+let elementEvent;
+let elementNum = 0;
+let offsetX, offsetY;
 
 elements.forEach((element) => {
-  element.addEventListener("dragstart", dragStart);
+  element.addEventListener("dragstart", (event) => {
+    dragStart(event)
+  });
 })
+board.addEventListener("dragover", (event) => {
+  dragOver(event)
+});
+board.addEventListener("drop", (event) => {
+  drop(event)
+});
 
-function dragOver(event) {
+const dragStart = (event) => {
+  elementEvent = event;
+}
+
+const dragOver = (event) => {
   event.preventDefault();
 }
 
-function dragStart(event) {
-  e = event;
-  event.dataTransfer.setData("src", event.target.src);
-  state = "library";
-}
-
-function boardDrag(event) {
-  e = event;
-  event.dataTransfer.setData("src", event.target.src);
-  state = "board";
-}
-
-function drop(event) {
+const drop = (event) => {
   event.preventDefault();
-  if (!state) return;
+  if (!elementEvent) return;
 
-  const rect = e.target.getBoundingClientRect();
-  offsetX = e.clientX - rect.left;
-  offsetY = e.clientY - rect.top;
+  const rect = elementEvent.target.getBoundingClientRect();
+  offsetX = elementEvent.clientX - rect.left;
+  offsetY = elementEvent.clientY - rect.top;
 
-  const relativeX = event.pageX - board.offsetLeft - offsetX;
-  const relativeY = event.pageY - board.offsetTop - offsetY;
+  const position = getSVGCoordinates(event.pageX - offsetX, event.pageY - offsetY);
+
+  const element = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+  element.setAttribute('x', position.x-15);
+  element.setAttribute('y', position.y);
+  element.setAttribute('class', "board__element_item");
+  element.setAttribute('data-id', elementNum);
+  elementNum += 1;
+
+  const wireL = document.createElementNS('http://www.w3.org/2000/svg', 'image');
+  wireL.setAttribute('href', '/static/images/wire.svg');
+  wireL.setAttribute('class', "board__element_wire");
+
+  const img = document.createElementNS('http://www.w3.org/2000/svg', 'image');
+  const src = elementEvent.target.src;
+  img.setAttribute('href', src);
+  img.style.height = "40px";
+  img.setAttribute('x', 15);
+
+  const wireR = wireL.cloneNode(true);
+  wireR.setAttribute('x', 55);
   
-  if (state=="board") {
-    e.target.style.left = `${relativeX}px`;
-    e.target.style.top = `${relativeY}px`;
-    state = false;
-    return;
-  }
+  element.append(wireL, img, wireR);
+  event.target.appendChild(element);
 
-  const img = document.createElement("img");
-  const src = event.dataTransfer.getData("src");
-  img.src = src;
-  img.className = "board__element_item";
-  
-  img.style.left = `${relativeX}px`;
-  img.style.top = `${relativeY}px`;
-
-  event.target.appendChild(img);
-  img.addEventListener("dragstart", boardDrag);
-  state = false;
+  elementEvent = null;
 }
+
