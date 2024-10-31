465f414a2d63136c34b65c9c16480062fdce4885
5tarry <hacherry05@naver.com>
2024-11-01 02:43:19 +0900

    ⚒️: 모델 추가

--- home/admin.py
+++ home/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Board, Component, Node, Connection
+from .models import Board, Component, Node, Connection, Wire
 
 # Register your models here.
 admin.site.register(Board)
 admin.site.register(Component)
+admin.site.register(Wire)
 admin.site.register(Node)
 admin.site.register(Connection)
