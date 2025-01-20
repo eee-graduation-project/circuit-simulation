a9c4707ed02903e67ddbb441280a27f0b8268406
5tarry <hacherry05@naver.com>
2025-01-20 21:49:43 +0900

    ⚒️: prettier 설정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,9 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
+import pluginPrettier from "eslint-plugin-prettier";
+import configPrettier from "eslint-config-prettier";
+import configAirbnb from "eslint-config-airbnb";
 
 
 /** @type {import('eslint').Linter.Config[]} */
@@ -9,4 +12,14 @@ export default [
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
+  configAirbnb,
+  {
+    plugins: {
+      prettier: pluginPrettier,
+    },
+    rules: {
+      "prettier/prettier": "error",
+    },
+  },
+  configPrettier,
 ];
